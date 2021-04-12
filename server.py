2b2eeefff09f2a8f13b66ec0345451593b37e0ad
Santi Bello <santidebello@gmail.com>
2021-04-12 23:16:27 +0200

    add doc error

--- server.py
+++ server.py
@@ -2,15 +2,15 @@ import requests
 from bot import telegram_chatbot
 
 bot = telegram_chatbot("config.cfg")
-
 btc = 'https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=849bd6e723cfcff8203a8b05cb2ddc08'
+update_id = None
 
 def make_reply(msg):
     reply = None
     if msg is not None:
         reply = "cosi"
     return reply
-update_id = None
+    
 while True:
     updates = bot.get_updates(offset=update_id)
     updates = updates["result"]
@@ -18,13 +18,12 @@ while True:
         for item in updates:
             update_id = item["update_id"]
             try:
-                message = str(item["message"]["text"])
+                message = str(item["message"]["text"]) # what you type on chat
             except:
                 message = None
-            print(message)
-            print("\n")
+                
             print(item)
-            from_ = 239266037 # my id
+            from_ = 239266037 # my telegram user id
             #from_ = item["message"]["from"]["id"]
             reply = make_reply(message)
             response = requests.get(btc) 
