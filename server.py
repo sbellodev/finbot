ab63f4abcb0ea142165cd927d273cb85db638779
Santi Bello <santidebello@gmail.com>
2021-05-11 18:37:52 +0200

    Changed API + Add IOT BTC

--- server.py
+++ server.py
@@ -1,8 +1,29 @@
 import requests
+import json
 from bot import telegram_chatbot
 
 bot = telegram_chatbot("config.cfg")
-btc = 'https://financialmodelingprep.com/api/v3/quote/BTCUSD?apikey=849bd6e723cfcff8203a8b05cb2ddc08'
+base_url = 'https://api.coinranking.com/v2/coin/' 
+coin_iot = 'LtWwuVANwRzV_' # miota
+coin_btc = 'Qwsogvtv82FCd' # btc
+coin_key = 'coinranking561c69335054fafd29f745fcd1592c26f2666ae0810bc273'
+
+
+def coin_reply(coin_url, message):
+    if message == "iot":
+        print("message is iot")
+        coin_url = coin_url + coin_iot
+    elif message == "btc":
+        print("message is btc")
+        coin_url = coin_url + coin_btc
+    else:
+        return "Error. Coin not found. Try again."
+    response = requests.get(coin_url, headers={'Authorization': coin_key}) 
+    d = json.loads(response.text)
+    #print(d.items())
+    data = d['data']['coin']
+    e = data['symbol'] + ' ' + data['name'] + ' \n Price: \t' + data['price'] + ' \n btcPrice: \t' + data['btcPrice'] + ' \n Change: \t' + data['change']
+    return e
 
 def make_reply(msg):
     reply = None
@@ -19,20 +40,17 @@ def runbot():
             for item in updates:
                 update_id = item["update_id"]
                 try:
-                    message = str(item["message"]["text"]) # what you type on chat
+                    message = str(item["message"]["text"]) # what you typed
                 except:
                     message = None
-
-                print(item)
                 from_ = 239266037 # enabled for single user 
-                #from_ = item["message"]["from"]["id"] # enabled for everyone
-                #reply = make_reply(message)
-                response = requests.get(btc) 
+                #from_ = item["message"]["from"]["id"] # enabled for 
                 if message == "help":
                     bot.send_message('Hi how can I help uwu?', from_)
-                if message == "uwu":
+                elif message == "uwu":
                     bot.send_message('heheh ewe', from_)
+                elif message == "iot" or message == "btc":
+                    bot.send_message(coin_reply(base_url, message), from_)
                 else:
-                    bot.send_message(response.text+' 🚀', from_)
-                #bot.send_message(reply, from_)
+                    bot.send_message("Type btc or iot\n or help uwu", from_)
 runbot()
