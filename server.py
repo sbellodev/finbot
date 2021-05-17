d3b5687d312ec9a3151e35483a94b6e9ab0fb2ef
Santi Bello <santidebello@gmail.com>
2021-05-17 23:40:19 +0200

    Changes percentajes to prices

--- server.py
+++ server.py
@@ -8,41 +8,46 @@ coin_iot = 'LtWwuVANwRzV_' # miota
 coin_btc = 'Qwsogvtv82FCd' # btc
 coin_key = 'coinranking561c69335054fafd29f745fcd1592c26f2666ae0810bc273'
 
-def coin_reply(coin_url, mode="manual", only_mode="disabled"):
+def coin_reply(coin_url, mode="manual", only_mode="default"):
     response = requests.get(coin_url, headers={'Authorization': coin_key}) 
     d = json.loads(response.text)
     #print(d.items())
     data = d['data']['coin']
     e = data['symbol'] + ' ' + data['name'] + ' \n Price: \t' + data['price'] + ' \n btcPrice: \t' + data['btcPrice'] + ' \n Change: \t' + data['change']
+    sell_low = 2.2
+    sell_high = 2.3
+    buy_low = 1.6
+    buy_high = 1.7
+
     if mode == "manual":
         return e
     elif mode == "auto":
-        if only_mode == "disabled":
-            if float(data['change']) < -10:
+        if only_mode == "default":
+            if float(data['price']) < buy_low:
                 return e + '\n > BUY TIME 🌕🌕'
-            elif float(data['change']) < -6:
+            elif float(data['price']) < buy_high:
                 return e + '\n > BUY TIME 🌕🌑'
-            elif float(data['price']) > 2.3:
+            elif float(data['price']) > sell_high:
                 return e + '\n > SELL TIME 🌕🌕'
-            elif float(data['price']) > 2.2:
+            elif float(data['price']) > sell_low:
                 return e + '\n > SELL TIME 🌕🌑'
             else: 
                 return ''
         elif only_mode == "buymode":
-            if float(data['change']) < -10:
+            if float(data['price']) < buy_low:
                 return e + '\n > BUY TIME 🌕🌕'
-            elif float(data['change']) < -6:
+            elif float(data['price']) < buy_high:
                 return e + '\n > BUY TIME 🌕🌑'
             else:
                 return ''
         elif only_mode == 'sellmode':
-            if float(data['price']) > 2.3:
+            if float(data['price']) > sell_high:
                 return e + '\n > SELL TIME 🌕🌕'
-            elif float(data['price']) > 2.2:
+            elif float(data['price']) > sell_low:
                 return e + '\n > SELL TIME 🌕🌑'
             else: 
                 return ''
-        elif only_mode == 'nonemode':
+        elif only_mode == 'none':
             return ''
     else:
         return ''
@@ -56,7 +61,7 @@ def make_reply(msg):
 def runbot():
     update_id = None
     last_msg = None
-    only_mode = "disabled"
+    only_mode = "default"
     while True:
         updates = bot.get_updates(offset=update_id)
         updates = updates["result"]
@@ -70,7 +75,7 @@ def runbot():
                 from_ = 239266037 # enabled for single user 
                 #from_ = item["message"]["from"]["id"] # enabled for 
                 if message == "help":
-                    bot.send_message('Supported commands:\n btc: shows bitcoin prices \n iot: shows miota/iota prices\n all: shows all supported coins prices\n buymode or sellmode: shows automatically only sell/buy prices \n disabled: disable only sell/buy prices \n help: shows help lol \n beep boop', from_)
+                    bot.send_message('beep boop ... \n Supported commands:\n btc: shows bitcoin prices \n iot: shows miota/iota prices\n all: shows all supported coins prices\n buymode or sellmode: shows automatically only sell/buy prices \n none: doesnt show anything \n default: shows b/s prices \n help: shows help lol \n beep boop', from_)
                 elif message == "uwu":
                     bot.send_message('heheh ewe', from_, )
                 elif message == "iot":
@@ -80,15 +85,15 @@ def runbot():
                 elif message == "all":
                     all_coins = coin_reply(base_url+coin_btc) + "\n\n" +  coin_reply(base_url+coin_iot)
                     bot.send_message(all_coins, from_)
-                elif message == "sellmode" or message == "buymode" or message == "nonemode" or message == "disabled" :
+                elif message == "sellmode" or message == "buymode" or message == "none" or message == "default" :
                     only_mode = message
                     bot.send_message("Mode changed to " + only_mode, from_)
                 else:
-                    bot.send_message("Type 'btc', 'iot', 'all' or 'sellmode / buymode / disabled' \n Current mode: "+only_mode, from_)
+                    bot.send_message("> 'btc', 'iot', 'all'  for crypto prices \n > 'sellmode / buymode / none / default' for filtering \n > current mode: "+only_mode+ "\n 'help' for details", from_)
         else:
             api_coin = coin_reply(base_url+coin_iot, "auto", only_mode) or ''
             if api_coin and api_coin != last_msg:
-                bot.send_message(api_coin, 239266037) # Only in development mode
+                bot.send_message(api_coin, 239266037)
             last_msg = api_coin
 
             #all_coins = coin_reply(base_url+coin_btc, "auto") + "\n" +  coin_reply(base_url+coin_iot, "auto") # add all coins
